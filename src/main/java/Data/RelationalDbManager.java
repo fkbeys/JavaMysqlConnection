a7eafe0b6cee61073936107b1e1c4bde0d472b25
F.Kaya <fkbeys@gmail.com>
2023-06-28 20:57:10 +0200

    some db work

--- src/main/java/Data/RelationalDbManager.java
+++ src/main/java/Data/RelationalDbManager.java
@@ -1,48 +1,50 @@
 package Data;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 
 public class RelationalDbManager {
-
     private String ip;
     private String username = "kaya";
     private String password = "18821882";
     private String dbname;
     private int port;
-
     private Statement statement = null;
-
-
     private Connection conn = null;
 
-    public void Connect() {
+    private void Commit() {
+        try {
+            conn.commit();
+        } catch (SQLException e) {
+            System.out.println("Sql transaction commit error:" + e.getMessage());
+        }
+    }
 
-        String preurl = "tjdbc:mysql://" + ip + ":" + port + "/" + dbname;
-        String url = "jdbc:mysql://64.226.113.128:3306/maindb";
+    private void RollBack() {
+        try {
+            conn.rollback();
+        } catch (SQLException e) {
+            System.out.println("Sql Rollback error:" + e.getMessage());
+        }
+    }
+
+    public void Connect() {
+        //String preurl = "tjdbc:mysql://" + ip + ":" + port + "/" + dbname;
+        String url = "jdbc:mysql://64.226.113.128:3306/maindb?useUnicode=true&characterEncoding=utf8";
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
+            conn = DriverManager.getConnection(url, username, password);
+            conn.setAutoCommit(false);
+            System.out.println("Connection Success.");
         } catch (Exception e) {
             System.out.println("Mysql driver can not found! ");
         }
-
-        try {
-            conn = DriverManager.getConnection(url, username, password);
-            System.out.println("Connection Success.");
-        } catch (SQLException e) {
-            System.out.println("Connection Error!");
-            System.out.println(e.getMessage());
-        }
-
-
     }
 
     public void GetData() {
 
         try {
-            String query = "select * from Car ";
+            String query = "select * from Car";
+
             statement = conn.createStatement();
             var data = statement.executeQuery(query);
             while (data.next()) {
@@ -56,5 +58,49 @@ public class RelationalDbManager {
 
     }
 
+    public void InsertData(int id, String name) {
+        try {
+            String insertQuery = "insert into Car (id,name) values ('" + id + "','" + name + "' ) ";
+            statement = conn.createStatement();
+            statement.executeUpdate(insertQuery);
+            Commit();
 
+            System.out.println("insert operation success.");
+        } catch (SQLException e) {
+            RollBack();
+            System.out.println("error on insert operation:" + e.getMessage());
+        }
+
+    }
+
+    public void UpdateData(int id, String name) {
+
+
+        try {
+            String updateQuery = "update Car set  name='" + name + "' where id='" + id + "' ";
+            statement = conn.createStatement();
+            statement.executeUpdate(updateQuery);
+            conn.commit();
+            System.out.println("update operation success.");
+        } catch (SQLException e) {
+            RollBack();
+            System.out.println("error on update operation:" + e.getMessage());
+        }
+
+    }
+
+    public void DeleteData(int id) {
+
+        try {
+            String deleteQuery = "delete from Car where id='" + id + "' ";
+            statement = conn.createStatement();
+            statement.executeUpdate(deleteQuery);
+            Commit();
+            System.out.println("delete operation success.");
+        } catch (SQLException e) {
+            RollBack();
+            System.out.println("error on delete operation:" + e.getMessage());
+        }
+
+    }
 }
--- src/main/java/org/javakaya/Main.java
+++ src/main/java/org/javakaya/Main.java
@@ -5,9 +5,10 @@ import Data.RelationalDbManager;
 public class Main {
     public static void main(String[] args) {
 
-        RelationalDbManager cc=new RelationalDbManager();
+        RelationalDbManager cc = new RelationalDbManager();
         cc.Connect();
+        cc.InsertData(1, "Mercedes");
+        //cc.DeleteData(1);
         cc.GetData();
-
     }
 }
